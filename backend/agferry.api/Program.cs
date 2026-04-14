b586b33ed96c008a1d91a620607dfbb4f5ded2cb
Pham Minh Tuan <phamminhtuanlx87n@gmail.com>
2026-04-14 14:45:48 +0700

    test deloy

--- backend/agferry.api/Program.cs
+++ backend/agferry.api/Program.cs
@@ -110,5 +110,6 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Run($"http://0.0.0.0:{port}");
+//app.Run();
